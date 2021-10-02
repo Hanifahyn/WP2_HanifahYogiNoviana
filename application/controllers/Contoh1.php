89b32f464663c5d283ce5607baa78843fab797b3
Hanifahyn <hanifahvivi71@gmail.com>
2021-10-02 14:02:08 +0700

    commit pertama

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,11 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo"Nama saya Hanifah Yogi Noviana
+            Saya tinggal di daerah Babelan
+            olahraga yang saya sukai adalah Bulutangkis";
+    }
+}
